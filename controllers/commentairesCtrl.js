e03cfd0aa7313ac62d8c9eae3a97d9b5b7ebd7eb
Colli Gregory <colli.gregory.13@gmail.com>
2022-02-15 16:08:33 +0100

    feat: ajout commentaire ctrl route

--- controllers/commentairesCtrl.js
+++ controllers/commentairesCtrl.js
@@ -15,17 +15,65 @@ exports.getCommentaire = (req, res) => {
 
 //Retourne tous les commentaires
 exports.getAll = (req, res) => {
-    
+    commentaireModel.findAll().then(commentaires => {
+        res.status(200).json(commentaires)
+    }).catch(error => res.status(500).json({ error }))
 }
 
 //Creer commentaire
 exports.create = (req, res) => {
 
+    const userId = req.auth.userId
+    const postId = req.body.postId
+
+    const commentaire = {
+        contenu: req.body.contenu,
+        userId: userId,
+        postId: postId
+    }
+    console.log(commentaire);
+    commentaireModel.create(commentaire)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Erreur lors de la création du commentaire."
+            });
+        })
+
 }
 
 //update commentaire
 exports.update = (req, res) => {
+    const id = req.params.id;
 
+    const commentaire = {
+      ...req.body
+    }
+
+    console.log(commentaire);
+
+    commentaireModel.update(commentaire, {
+        where: { id: id }
+      })
+        .then(num => {
+          if (num == 1) {
+            res.send({
+              message: "Commentaire modifié avec succès."
+            });
+          } else {
+            res.send({
+              message: `Impossible de modifier le commentaire avec id=${id}. Peut etre introuvable!`
+            });
+          }
+        })
+        .catch(err => {
+          res.status(500).send({
+            message: "Impossible de modifier le commentaire avec id=" + id
+          });
+        });
 }
 
 //delete commentaire
