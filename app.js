f76223fecbda30a3f94bb0a6d0729d02bb3f717a
Colli Gregory <colli.gregory.13@gmail.com>
2022-02-24 15:30:30 +0100

    fix: bug sur visualisation image

--- app.js
+++ app.js
@@ -91,7 +91,8 @@ const commentairesRoutes = require('./routes/commentairesRoutes');
 
 // const likesRoutes = require('./routes/likesRoutes');
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images/utilisateurs', express.static(path.join(__dirname, 'images')));
+app.use('/images/posts', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/utilisateur', utilisateurRoutes);
 app.use('/api/post', postRoutes);
