f0defcfa80d8a30db1faba3612a526000717e081
Colli Gregory <colli.gregory.13@gmail.com>
2022-03-09 12:16:50 +0100

    essai deploiement heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,28 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "root",
+//     DB: "groupomania",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
+//SITE EN LIGNE
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "groupomania",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "eu-cdbr-west-02.cleardb.net",
+  USER: "b225a9ad0874bf",
+  PASSWORD: "9f1414a6",
+  DB: "heroku_86d2b01f8c98d97",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
